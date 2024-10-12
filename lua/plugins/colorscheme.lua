18c44f4f0d4a02063964070421c09a6769e5b450
CaueAnjos <kawidosanjos@outlook.com>
2024-10-12 19:15:10 -0300

    better plugins files

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,27 @@
 return {
   {
     "EdenEast/nightfox.nvim",
-    lazy = true, -- load immediately
-    name = "nightfox",
-    config = function()
-      local nightfox = require("nightfox")
-      nightfox.setup({
-        fox = "nordfox", -- Which fox style should be applied
-        transparent = true, -- Disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening :terminal
-        styles = {
-          comments = "italic",
-          keywords = "bold",
-          functions = "bold, italic",
-          variables = "NONE",
-          strings = "NONE",
-        },
-        inverse = {
-          match_paren = true,
-          visual = false,
-          search = true,
-        },
-        colors = {}, -- Override default colors
-        hlgroups = {}, -- Override highlight groups
-      })
-    end,
+    lazy = false, -- load immediately
+    priority = 1000,
+    opts = {
+      fox = "nordfox", -- Which fox style should be applied
+      transparent = true, -- Disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening :terminal
+      styles = {
+        comments = "italic",
+        keywords = "bold",
+        functions = "bold, italic",
+        variables = "NONE",
+        strings = "NONE",
+      },
+      inverse = {
+        match_paren = true,
+        visual = false,
+        search = true,
+      },
+      colors = {}, -- Override default colors
+      hlgroups = {}, -- Override highlight groups
+    },
   },
 
   -- Configure LazyVim to load nightfox
--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,14 +1,14 @@
 return {
   {
     "xiyaowong/nvim-transparent",
-    config = function()
-      require("transparent").setup({
-        enable = true,
-        extra_groups = {}, -- Add any additional groups you want here
-        exclude = {
-          "NeoTree", -- Exclude Neo Tree from transparency
-        },
-      })
-    end,
+    lazy = false, -- load immediately
+    priority = 1000,
+    opts = {
+      enable = true,
+      extra_groups = {}, -- Add any additional groups you want here
+      exclude = {
+        "NeoTree", -- Exclude Neo Tree from transparency
+      },
+    },
   },
 }
