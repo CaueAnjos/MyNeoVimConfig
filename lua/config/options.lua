0e884b0b1267cdcabf885a616a1334343d9889fa
CaueAnjos <kawidosanjos@outlook.com>
2025-02-11 11:38:03 -0300

    add xaml

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,8 @@ vim.opt.wrap = true
 
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
+
+vim.g.xmlformat_indent = 4
+vim.g.xmlformat_line_length = 80
+vim.g.xmlformat_wrap_attributes = 1
+vim.g.xmlformat_keep_spaces = 1
