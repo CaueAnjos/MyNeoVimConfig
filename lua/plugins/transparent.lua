99107e4b846478faa0aba8123ae01d3734f295a6
CaueAnjos <kawidosanjos@outlook.com>
2025-03-07 16:18:39 -0300

    solving transparent issuis with picker

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -5,7 +5,10 @@ return {
     priority = 1000,
     opts = {
       enable = true,
-      extra_groups = {}, -- Add any additional groups you want here
+      extra_groups = {
+        "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+        "NvimTreeNormal", -- NvimTree
+      }, -- Add any additional groups you want here
       exclude = {},
     },
   },
