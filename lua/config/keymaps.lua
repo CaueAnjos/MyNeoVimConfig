455fb1267d20644c1a9da9505423d4dd83170ccb
CaueAnjos <kawidosanjos@outlook.com>
2025-05-12 19:46:30 -0300

    add silent for mark keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps hereby
 
-vim.keymap.set("n", "ç", "'", { desc = "goto mark" })
+vim.keymap.set("n", "ç", "'", { desc = "Goto mark", silent = true })
+
 vim.keymap.set("n", "<leader>çx", function()
   local mark = vim.fn.input("Delete mark: ")
   if mark ~= "" then
     vim.cmd("delmarks " .. mark)
   end
-end, { desc = "Delete a specific mark" })
+end, { desc = "Delete a specific mark", silent = true })
