70cd273f70a27eb8615cba9f1635a7d5ba9fc54b
CaueAnjos <kawidosanjos@outlook.com>
2025-05-14 12:04:16 -0300

    changing tab width

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,5 +6,10 @@ vim.o.shell = "pwsh"
 
 vim.opt.wrap = true
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
